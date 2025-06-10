15f7572ec5fa0ea9fe624de67288818f937e56af
Joseph Emmanuel <117820245+Fmsticks2@users.noreply.github.com>
2025-06-10 19:36:38 +0100

    pages uodated and working MVP

--- src/components/WalletProvider.tsx
+++ src/components/WalletProvider.tsx
@@ -1,11 +1,19 @@
 import { AptosWalletAdapterProvider } from '@aptos-labs/wallet-adapter-react';
 import { MartianWallet } from '@martianwallet/aptos-wallet-adapter';
 import { PetraWallet } from 'petra-plugin-wallet-adapter';
+import { FewchaWallet } from 'fewcha-plugin-wallet-adapter';
+import { PontemWallet } from '@pontem/wallet-adapter-plugin';
+import { RiseWallet } from '@rise-wallet/wallet-adapter';
+import { MSafeWalletAdapter } from 'msafe-plugin-wallet-adapter';
 import { PropsWithChildren } from 'react';
 
 const wallets = [
   new MartianWallet(),
   new PetraWallet(),
+  new FewchaWallet(),
+  new PontemWallet(),
+  new RiseWallet(),
+  new MSafeWalletAdapter(),
 ];
 
 export function WalletProvider({ children }: PropsWithChildren) {
